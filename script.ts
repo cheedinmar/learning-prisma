2d25f0b49c2ef0c93fe02d4ee8f475789c2bee57
Blessing Onwunali <cheedinmarblessing@gmail.com>
2023-07-01 12:26:54 +0100

    features: data source and generators, defining models

--- script.ts
+++ script.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+//allows you to interact with your code with prisma library, so you don't have to write raw sql
 
+async function main (){
+    const users = await prisma.user.findMany()
+    console.log(users)
+    // ...write prisma client queries here
+}
+main()
+//     .catch(e =>{
+//         console.log(e.message)
+//     })
+//     .finally(async()=>{
+//         await prisma.$disconnect()
+//     })
