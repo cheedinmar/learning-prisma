a0dcf85c067f38fceb359ffb8eaa8e3807d36997
Blessing Onwunali <cheedinmarblessing@gmail.com>
2023-07-03 01:08:16 +0100

    feat: connecting existing relationship

--- script.ts
+++ script.ts
@@ -45,33 +45,52 @@ async function main() {
         //you can only do select || incude, can't do both at the same time
     // })
     //finUnique to find  properties with unique attributes
-    const user1 = await prisma.user.findMany({
-       where:{
-        OR:[
-           { email:{
-                startsWith:"Sally"
-            }},{
-                email:{
-                    endsWith:"@test.com"
-                }
-            }
-        ],
-        age:{
-            lt:100
-            //less than
-        },
-        email:{
-            // contains:"@test1"
-             endsWith: "@test.com"
-        }
-            // name: { not : "Sally"}
-            // email:"sally@test.com",
-       } ,
-       orderBy:{
-        age:"desc"
-       }
-    //    take:1
+    // const user1 = await prisma.user.findMany({
+    //    where:{
+    //     OR:[
+    //        { email:{
+    //             startsWith:"Sally"
+    //         }},{
+    //             email:{
+    //                 endsWith:"@test.com"
+    //             }
+    //         }
+    //     ],
+    //     age:{
+    //         lt:100
+    //         //less than
+    //     },
+    //     email:{
+    //         // contains:"@test1"
+    //          endsWith: "@test.com"
+    //     }
+    //         // name: { not : "Sally"}
+    //         // email:"sally@test.com",
+    //    } ,
+    //    orderBy:{
+    //     age:"desc"
+    //    }
+    // //    take:1
 
+    // })
+
+    // const preferences = await prisma.userPreferences.create({
+    //     data:{
+    //         emailUpdates: true
+    //     }
+    // })
+    //update will update the first user it finds, and updateMany will update all users
+    const user1 = await prisma.user.update({
+        where:{
+            email:"kyhle@test.com"
+        },
+        data:{
+           userPreference:{
+            connect:{
+                   id:"4dec53da-1501-4908-a242-36b8d64d0891"
+            }
+           }
+        }
     })
     console.log(user1)
     // ...write prisma client queries here
